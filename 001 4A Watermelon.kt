9bbf0deb1d4e725d2d44ef6bfe4243a553361692
caffs <aburaksahin@hotmail.com.tr>
2024-02-14 17:12:40 +0300

    Codeforces 4A Watermelon
    4A - Watermelon solution with Kotlin

---
+++ 001 4A Watermelon.kt
@@ -0,0 +1,10 @@
+//4A - Watermelon solution with Kotlin
+//Codeforces 001
+fun main() {
+    val w : Int = Integer.valueOf(readLine())
+    if ((2 < w) && ((w % 2) == 0)){
+        println("YES")
+    }else{
+        println("NO")
+    }
+} 
